53540b19082b15f4047b8c19ffd3cc1c15117e34
LuK1337 <priv.luk@gmail.com>
2023-11-01 18:11:13 +0100

    sdm660-common: Use libutils-v33 for pm-service
    Change-Id: Ia998fa72d8747de18010abe6491d2c374cb7aa0a
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -447,6 +447,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.fastcharge@1.0-service.qcom
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
+
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(COMMON_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
